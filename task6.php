1439e4d8c0fe7cc730d532cc5ba12d0ee6997495
TasnimShakal <102739791+TasnimShakal@users.noreply.github.com>
2022-06-07 16:23:20 +0600

    Shakal

---
+++ task6.php
@@ -0,0 +1,14 @@
+<?php
+$dessert=['cupcakes','chocolate mousse','swissroll','cheesecake','jarcake']; 
+
+foreach($dessert as $x => $val) {
+  
+    if($val=='cheesecake'){
+      
+         echo "$x = $val<br>";
+         
+
+    }
+   
+  }
+?>
